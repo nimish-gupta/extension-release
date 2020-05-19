004bc948002f132347ad27f733b6ae5896e037ff
Nimish <nimishgupta18396@gmail.com>
2020-05-19 15:42:10 +0530

    feat: added the release github plugin

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ const { Future } = require('ramda-fantasy');
 const parseArgs = require('minimist');
 const fs = require('fs');
 const path = require('path');
-const { exec } = require('child_process');
+const release = require('release-github');
 
-const manifestExists = (argPath) =>
+const manifestExists = (args) =>
 	new Future((reject, resolve) => {
-		const filePath = path.join(__dirname, argPath);
+		const filePath = path.join(__dirname, args.path);
 		return fs.exists(filePath, (isExist) =>
 			isExist
-				? resolve(filePath)
+				? resolve({ ...args, manifestPath: filePath })
 				: reject(
 						`manifest.json doesn't exist. Please provide the correct path using --path argument`
 				  )
 		);
 	});
 
-const getExtensionVersion = (filePath) =>
+const getExtensionVersion = (args) =>
 	new Future((reject, resolve) =>
-		fs.readFile(filePath, {}, (err, data) => {
+		fs.readFile(args.manifestPath, {}, (err, data) => {
 			if (err) {
 				return reject(`Could not read the file, due to ${err}`);
 			}
@@ -29,7 +29,7 @@ const getExtensionVersion = (filePath) =>
 				if (version === undefined) {
 					throw new Error('Version property not found in manifest');
 				}
-				return resolve(version);
+				return resolve({ ...args, releaseVersion: version });
 			} catch (error) {
 				return reject(
 					`manifest.json could not be parsed, due to ${error.message}`
@@ -54,61 +54,23 @@ const getArgs = (args) =>
 			path: 'manifest.json',
 		},
 	});
-const doesTagExist = (msg) =>
-	msg.indexOf('No names found, cannot describe anything.') !== -1;
 
-const matchTag = (version) =>
+const createGitRelease = (args) =>
 	new Future((reject, resolve) =>
-		exec('git describe', (err, stdout) => {
-			if (err instanceof Error) {
-				if (doesTagExist(err.message)) {
-					resolve({ latest: undefined, version });
-				} else {
-					reject(err);
-				}
-			} else {
-				resolve({ latest: stdout.trim(), version });
-			}
+		release({
+			repo: 'extension-release',
+			owner: 'nimish-gupta',
+			releaseVersion: args.releaseVersion,
+			open: true,
 		})
+			.then(resolve)
+			.catch(reject)
 	);
-
-const getCommitRange = ({ latest, version }) =>
-	new Future(async (reject, resolve) => {
-		let start = latest,
-			end = `v${version}`;
-		if (latest === undefined) {
-			return exec('git rev-list --max-parents=0 HEAD', (err, stdout) => {
-				if (err instanceof Error) {
-					reject(err);
-				} else {
-					resolve({ start: stdout.trim(), end });
-				}
-			});
-		}
-		resolve({ start, end });
-	});
-
-const createEndTag = ({ start, end }) =>
-	new Future((reject, resolve) =>
-		exec(`git tag -a ${end} -m ${end}`, (err) => {
-			if (err instanceof Error) {
-				reject(err);
-			} else {
-				resolve({ start, end });
-			}
-		})
-	);
-
-const createGitRelease = () => {};
 
 const main = R.compose(
 	R.chain(createGitRelease),
-	R.chain(createEndTag),
-	R.chain(getCommitRange),
-	R.chain(matchTag),
 	R.chain(getExtensionVersion),
 	R.chain(manifestExists),
-	R.map(R.prop('path')),
 	R.map(getArgs),
 	checkGitExists
 );
